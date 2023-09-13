c6565dec74aa7b111a23e072ac46f34ac4cc6a6b
BrunnoSM <brunno.miotto@gmail.com>
2023-09-13 17:38:48 -0300

    First Commit

---
+++ EmployeeData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeSalary
+{
+    class EmployeeData
+    {
+        public string Name { get; set; } = string.Empty;
+        public double Salary { get; set; }
+        public double Tax { get; set; }
+
+        // Salário Líquido
+        public double NetSalary()
+        {
+            return Salary - Tax;
+        }
+
+        // Aplicar Aumento Conforme a Porcentagem Indicada por Parâmetro
+        public void ApplyIncrease(double applyIncreaseSalary)
+        {
+            double increase = Salary * (applyIncreaseSalary / 100);
+            Salary += increase;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, $ {NetSalary().ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using EmployeeSalary;
+
+namespace SalaryWorkersIncrease
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            EmployeeData workerInfo = new EmployeeData();
+
+            Console.Write("Nome: ");
+            workerInfo.Name = Console.ReadLine()!;
+
+            Console.Write("Salário Bruto: ");
+            workerInfo.Salary = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+
+            Console.Write("Imposto: ");
+            workerInfo.Tax = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.WriteLine($"Funcionário: {workerInfo}");
+
+            Console.WriteLine();
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            double increaseSalary = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+
+            // Aplica o Aumento
+            workerInfo.ApplyIncrease(increaseSalary);
+
+            // Dados Atualizado
+            Console.WriteLine();
+            Console.WriteLine($"Dados atualizados: {workerInfo}");
+
+        }
+    }
+}
